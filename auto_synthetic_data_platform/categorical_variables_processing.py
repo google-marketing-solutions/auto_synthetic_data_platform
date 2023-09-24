98157387ce28c0e4a1a5025e504eaf7b8826d70c
Kacper Krasowiak <krasowiak@google.com>
2023-09-24 11:09:42 +0000

    feat: Added check_categorical_column function with tests.
    Change-Id: Iad4fc2ad1d9278e6b4023fc1c7fd224f8862be19

--- auto_synthetic_data_platform/categorical_variables_processing.py
+++ auto_synthetic_data_platform/categorical_variables_processing.py
@@ -6,6 +6,7 @@ import pandas as pd
 _MILD_IMBALANCE_UPPER_LIMIT: Final[float] = 0.4
 _MILD_IMBALANCE_LOWER_LIMIT: Final[float] = 0.2
 _MODERATE_IMBALANCE_LOWER_LIMIT: Final[float] = 0.01
+_RECOMMENDED_MAXIMUM_DIMENSIONALITY: Final[int] = 1000
 
 
 def check_for_imbalance(
@@ -70,3 +71,41 @@ def check_for_imbalance(
         " variables: %s."
         % (categorical_column_name, ", ".join(imbalance_class_names))
     )
+
+
+def check_categorical_column(
+    *,
+    categorical_column_name: str,
+    categorical_column_data: pd.Series,
+    logger: logging.Logger,
+) -> None:
+  """Runs checks on a categorical column.
+
+  Args:
+    categorical_column_name: A categorical column name.
+    categorical_column_data: A categorical column data.
+    logger: A logger object to log and save preprocessing messages.
+  """
+  column_dimensions = categorical_column_data.nunique()
+  if column_dimensions > _RECOMMENDED_MAXIMUM_DIMENSIONALITY:
+    logger.warning(
+        "The categorical column '%s' has a higher dimensionality than"
+        " recommended, %d > %r."
+        % (
+            categorical_column_name,
+            column_dimensions,
+            _RECOMMENDED_MAXIMUM_DIMENSIONALITY,
+        )
+    )
+  if column_dimensions < 50 or column_dimensions > 100:
+    logger.warning(
+        "The categorical column '%s' dimensionality is %s. The recommended"
+        " column cardinality range is 50-100. Real and synthetic distribution"
+        " metrics might get adversely affected."
+        % (categorical_column_name, column_dimensions)
+    )
+  check_for_imbalance(
+      categorical_column_name=categorical_column_name,
+      categorical_column_data=categorical_column_data,
+      logger=logger,
+  )
--- tests/categorical_variables_processing_test.py
+++ tests/categorical_variables_processing_test.py
@@ -4,6 +4,7 @@ import tempfile
 from absl.testing import parameterized
 from auto_synthetic_data_platform import categorical_variables_processing
 from auto_synthetic_data_platform import experiment_logging
+import numpy as np
 import pandas as pd
 
 
@@ -39,6 +40,40 @@ class CategoricalVariablesProcessingTests(parameterized.TestCase):
           logger_name="test",
       )
       categorical_variables_processing.check_for_imbalance(
+          categorical_column_name="categorical_column",
+          categorical_column_data=test_dataframe["categorical_column"],
+          logger=logger,
+      )
+      log_path = pathlib.Path(temporary_directory).joinpath(
+          "logs/test_logs.log"
+      )
+      with log_path.open() as logs:
+        logged_message = logs.readlines()
+      self.assertTrue(expected_log_excerpt in logged_message[0])
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="exceeds_recommended_maximum_dimensionality",
+          test_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": np.arange(0, 1001),
+          }),
+          expected_log_excerpt="has a higher dimensionality than",
+      ),
+      dict(
+          testcase_name="unrecommended_cardinality",
+          test_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": np.arange(0, 49),
+          }),
+          expected_log_excerpt="recommended column cardinality range is 50-100",
+      ),
+  )
+  def test_check_categorical_column(self, test_dataframe, expected_log_excerpt):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      categorical_variables_processing.check_categorical_column(
           categorical_column_name="categorical_column",
           categorical_column_data=test_dataframe["categorical_column"],
           logger=logger,
