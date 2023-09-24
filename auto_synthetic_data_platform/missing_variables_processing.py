93973aadad5b75fa1a3a6baa39b963e9a9a834d1
Kacper Krasowiak <krasowiak@google.com>
2023-09-24 11:19:25 +0000

    feat: Added the missing_variables_processing module with tests.
    Change-Id: I298721987d2910399fadf18eb360d066d96d1d65

---
+++ auto_synthetic_data_platform/missing_variables_processing.py
@@ -0,0 +1,61 @@
+"""A missing values processing module of the Google EMEA gPS Data Science Auto Synthetic Data Platform."""
+import logging
+import numpy as np
+import pandas as pd
+from typing import Literal
+
+
+def list_columns_with_missing_values(*, dataframe: pd.DataFrame) -> list[str]:
+  """Returns an sequence of columns with missing values in the dataframe.
+
+  Args:
+    dataframe: A dataframe to check for missing values.
+  """
+  dataframe.replace("", np.nan, inplace=True)
+  return dataframe.columns[dataframe.isnull().any()].tolist()
+
+
+def process_missing_values(
+    *,
+    dataframe: pd.DataFrame,
+    missing_values_preprocessing_method: Literal["drop", "mode"],
+    logger: logging.Logger,
+) -> pd.DataFrame:
+  """Processes missing values from the dataframe using the specified method.
+
+  Args:
+    dataframe: A dataframe with missing values.
+    missing_values_preprocessing_method: A preprocessing method describing how
+      to process missing values. Possible methods are "drop" and "mode".
+    logger: A logger object to log and save preprocessing messages.
+
+  Returns:
+    A dataframe with processed missing values.
+
+  Raises:
+   ValueError: An error if a user tries to use an unknown
+    'missing_values_preprocessing_method'.
+  """
+  columns_with_missing_values = list_columns_with_missing_values(
+      dataframe=dataframe
+  )
+  if not columns_with_missing_values:
+    logger.info("No columns with missing values detected.")
+    return dataframe
+  logger.warning(
+      "The input dataframe has missing values in columns: %s."
+      % ", ".join(columns_with_missing_values)
+  )
+  if missing_values_preprocessing_method == "drop":
+    dataframe.dropna(axis=0, how="any", inplace=True)
+  elif missing_values_preprocessing_method == "mode":
+    dataframe.fillna(dataframe.mode().iloc[0], inplace=True)
+  else:
+    raise ValueError(
+        f"Unknown missing values method: {missing_values_preprocessing_method}"
+    )
+  logger.warning(
+      "Preprocesssing the missing values using the specified method: '%s'."
+      % missing_values_preprocessing_method
+  )
+  return dataframe
---
+++ tests/missing_variables_processing_test.py
@@ -0,0 +1,134 @@
+"""Tests for utility functions in missing_values_processing.py."""
+import pathlib
+import tempfile
+from absl.testing import absltest
+from absl.testing import parameterized
+from auto_synthetic_data_platform import experiment_logging
+from auto_synthetic_data_platform import missing_variables_processing
+import numpy as np
+import pandas as pd
+
+
+class MissingValuesProcessingTests(parameterized.TestCase):
+
+  def test_check_for_imbalance(self):
+    input_dataframe = pd.DataFrame.from_dict({
+        "categorical_column": [0, 1, 1, np.nan, 1, 1, 1, 1, 1, 1],
+    })
+    actual_output = missing_variables_processing.list_columns_with_missing_values(
+        dataframe=input_dataframe,
+    )
+    self.assertTrue(actual_output, ["categorical_column"])
+
+  def test_process_missing_values_no_missing_values(self):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      input_dataframe = pd.DataFrame.from_dict({
+          "categorical_column": [0, 1],
+          "numerical_column": [0.1, 0.2],
+      })
+      actual_output = missing_variables_processing.process_missing_values(
+          dataframe=input_dataframe,
+          missing_values_preprocessing_method="drop",
+          logger=logger,
+      )
+      dataframe_equal = actual_output.equals(input_dataframe)
+      expected_message = "No columns with missing values detected"
+      log_path = pathlib.Path(temporary_directory).joinpath(
+          "logs/test_logs.log"
+      )
+      with log_path.open() as logs:
+        logged_message = logs.readlines()
+      messages_equal = expected_message in logged_message[0]
+      self.assertTrue(all([dataframe_equal, messages_equal]))
+
+  @parameterized.named_parameters(
+      dict(
+          testcase_name="drop_method",
+          missing_values_preprocessing_method="drop",
+          input_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": [0, np.nan],
+              "numerical_column": [0.1, 0.2],
+          }),
+          output_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": [0.0],
+              "numerical_column": [0.1],
+          }),
+          expected_second_log_excerpt=(
+              "Preprocesssing the missing values using the specified method:"
+              " 'drop'."
+          ),
+      ),
+      dict(
+          testcase_name="unrecommended_cardinality",
+          missing_values_preprocessing_method="mode",
+          input_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": [0, np.nan],
+              "numerical_column": [0.1, 0.2],
+          }),
+          output_dataframe=pd.DataFrame.from_dict({
+              "categorical_column": [0.0, 0.0],
+              "numerical_column": [0.1, 0.2],
+          }),
+          expected_second_log_excerpt=(
+              "Preprocesssing the missing values using the specified method:"
+              " 'mode'."
+          ),
+      ),
+  )
+  def test_process_missing_values_no_missing_values(
+      self,
+      missing_values_preprocessing_method,
+      input_dataframe,
+      output_dataframe,
+      expected_second_log_excerpt,
+  ):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      actual_output = missing_variables_processing.process_missing_values(
+          dataframe=input_dataframe,
+          missing_values_preprocessing_method=missing_values_preprocessing_method,
+          logger=logger,
+      )
+      dataframe_equal = actual_output.equals(output_dataframe)
+      log_path = pathlib.Path(temporary_directory).joinpath(
+          "logs/test_logs.log"
+      )
+      with log_path.open() as logs:
+        logged_message = logs.readlines()
+      first_message_equal = (
+          "The input dataframe has missing values in columns:"
+          " categorical_column."
+          in logged_message[0]
+      )
+      second_message_equal = expected_second_log_excerpt in logged_message[1]
+      self.assertTrue(
+          all([dataframe_equal, first_message_equal, second_message_equal])
+      )
+
+  def test_process_missing_values_value_error(self):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      input_dataframe = pd.DataFrame.from_dict({
+          "categorical_column": [0, np.nan],
+          "numerical_column": [0.1, 0.2],
+      })
+      with self.assertRaisesRegex(ValueError, "Unknown missing values method"):
+        missing_variables_processing.process_missing_values(
+            dataframe=input_dataframe,
+            missing_values_preprocessing_method="unknown_method",
+            logger=logger,
+        )
+
+
+if __name__ == "__main__":
+  absltest.main()
