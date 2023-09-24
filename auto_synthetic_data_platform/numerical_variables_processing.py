7b062d1c03adb6850b6c212cdf03cb592c788fb5
Kacper Krasowiak <krasowiak@google.com>
2023-09-24 11:22:37 +0000

    feat: Added the numerical_variables_processing module with tests.
    Change-Id: I1db05ed7369e8d39c20c8cc98de2f505b2bb5140

---
+++ auto_synthetic_data_platform/numerical_variables_processing.py
@@ -0,0 +1,63 @@
+"""A numerical values processing module of the Google EMEA gPS Data Science Auto Synthetic Data Platform."""
+import logging
+from typing import Any, Mapping
+import numpy as np
+import pandas as pd
+
+
+def remove_column_outliers(
+    *,
+    dataframe: pd.DataFrame,
+    column_name: str,
+    lower_percentile: float = 0.5,
+    upper_percentile: float = 99.5,
+) -> pd.DataFrame:
+  """Returns a dataframe with removed rows containing the column's outliers.
+
+  Args:
+    dataframe: A dataframe to check for outliers.
+    column_name: A numerical column name to check for outliers.
+    lower_percentile: A lower precentile boundary. All the values below the
+      lower limit marked by this percentile will be removed.
+    upper_percentile: An upper precentile boundary. All the values over the
+      upper limit marked by this percentile will be removed.
+  """
+  lower_limit, upper_limit = np.percentile(
+      a=dataframe[column_name], q=[lower_percentile, upper_percentile]
+  )
+  return dataframe[
+      (dataframe[column_name] > lower_limit)
+      & (dataframe[column_name] < upper_limit)
+  ]
+
+
+def process_numerical_columns(
+    *,
+    dataframe: pd.DataFrame,
+    column_metadata: Mapping[str, list[Any]],
+    remove_numerical_outliers: bool,
+    logger: logging.Logger,
+) -> pd.DataFrame:
+  """Preprocesses numerical columns of the dataframe.
+
+  Args:
+    dataframe: A dataframe for the preprocessing.
+    column_metadata: A mapping between "categorical" and "numerical" data types
+      and column names.
+    remove_numerical_outliers: An indicator if to remove numerical outliers from
+      the numeric columns.
+    logger: A logger object to log and save preprocessing messages.
+
+  Returns:
+    A dataframe with processed numerical columns.
+  """
+  numerical_column_names = column_metadata.get("numerical", None)
+  if not numerical_column_names:
+    logger.info("No numerical columns are specified for the dataframe.")
+    return dataframe
+  if remove_numerical_outliers:
+    for column_name in numerical_column_names:
+      dataframe = remove_column_outliers(
+          dataframe=dataframe, column_name=column_name
+      )
+  return dataframe
---
+++ tests/numerical_variables_processing_test.py
@@ -0,0 +1,66 @@
+"""Tests for utility functions in experiment_logging.py."""
+import pathlib
+import tempfile
+from absl.testing import absltest
+from auto_synthetic_data_platform import experiment_logging
+from auto_synthetic_data_platform import numerical_variables_processing
+import pandas as pd
+
+_DATAFRAME = pd.DataFrame.from_dict({
+    "categorical_column": [1, 2, 3, 4],
+    "numerical_column": [1.01, 2.91, 3.05, 1000000],
+})
+_EXPECTED_DATAFRAME = pd.DataFrame.from_dict(
+    {"categorical_column": [2, 3], "numerical_column": [2.91, 3.05]}
+)
+
+
+class NumericalValuesProcessingTests(absltest.TestCase):
+
+  def test_remove_column_outliers(self):
+    actual_output = numerical_variables_processing.remove_column_outliers(
+        dataframe=_DATAFRAME, column_name="numerical_column"
+    ).reset_index(drop=True)
+    pd.testing.assert_frame_equal(actual_output, _EXPECTED_DATAFRAME)
+
+  def test_process_numerical_columns_no_numerical_columns(self):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      column_metadata = {"categorical": ["categorical_column"]}
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      actual_output = numerical_variables_processing.process_numerical_columns(
+          dataframe=_DATAFRAME,
+          column_metadata=column_metadata,
+          remove_numerical_outliers=True,
+          logger=logger,
+      )
+      dataframe_equal = actual_output.equals(_DATAFRAME)
+      expected_message = "No numerical columns are specified for the dataframe."
+      log_path = pathlib.Path(temporary_directory).joinpath(
+          "logs/test_logs.log"
+      )
+      with log_path.open() as logs:
+        logged_message = logs.readlines()
+      messages_equal = expected_message in logged_message[0]
+      self.assertTrue(all([dataframe_equal, messages_equal]))
+
+  def test_process_numerical_columns_with_numerical_columns(self):
+    with tempfile.TemporaryDirectory() as temporary_directory:
+      column_metadata = {"numerical": ["numerical_column"]}
+      logger = experiment_logging.setup_logger(
+          experiment_directory=pathlib.Path(temporary_directory),
+          logger_name="test",
+      )
+      actual_output = numerical_variables_processing.process_numerical_columns(
+          dataframe=_DATAFRAME,
+          column_metadata=column_metadata,
+          remove_numerical_outliers=True,
+          logger=logger,
+      ).reset_index(drop=True)
+      pd.testing.assert_frame_equal(actual_output, _EXPECTED_DATAFRAME)
+
+
+if __name__ == "__main__":
+  absltest.main()
